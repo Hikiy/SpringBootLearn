879047ecc688a731fd4c12177224007a31fbeaa0
hikiy <414950815@qq.com>
2019-05-07 17:40:20 +0800

    初步搭建SpringBoot框架，测试多环境配置

--- springbootlearn/src/main/java/com/hiki/springbootlearn/SpringbootlearnApplication.java
+++ springbootlearn/src/main/java/com/hiki/springbootlearn/SpringbootlearnApplication.java
@@ -1,5 +1,7 @@
 package com.hiki.springbootlearn;
 
+import com.hiki.springbootlearn.entity.AuthorSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class SpringbootlearnApplication {
-    @Value("${learn.author}")
-    private String learnAuthor;
-    @Value("${learn.name}")
-    private String learnName;
+//    @Value("${learn.author}")
+//    private String learnAuthor;
+//    @Value("${learn.name}")
+//    private String learnName;
+    @Autowired
+    private AuthorSettings authorSettings;
 
-    @RequestMapping("/")
-    String index(){
-        return "learn name is:"+learnName+",and learn author is:"+learnAuthor;
+//    @RequestMapping("/")
+//    String index(){
+//        return "learn name is:"+learnName+",and learn author is:"+learnAuthor;
+//    }
+
+    @RequestMapping("/authorsetting")
+    String authorsetting(){
+        return "name is "+authorSettings.getName()+" age is "+authorSettings.getAge();
     }
 
     public static void main(String[] args) {
